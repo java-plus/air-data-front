e3f2a8d7af4fbf6b86bf1406a7400f623fb76c0f
Kévin SEGUINEAU <kevinseguineau+git@gmail.com>
2019-10-01 17:05:43 +0200

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  navbarOpen = false;
+
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+    console.log('ok');
+  }
 }
