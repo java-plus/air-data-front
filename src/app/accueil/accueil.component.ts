fae94475b03a26effce040c63194547ffeb3b250
Olivier LASBLEIS <lasbleis.olivier@yahoo.fr>
2019-10-02 11:57:29 +0200

    responsive composant accueil

--- src/app/accueil/accueil.component.ts
+++ src/app/accueil/accueil.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccueilComponent implements OnInit {
 
+  clickSurCarte:boolean=false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  methodeClickSurCarte(){
+    this.clickSurCarte=true;
   }
 
 }
