55577451dab01ed92a977707f21acf8db6fe789f
Olivier LASBLEIS <lasbleis.olivier@yahoo.fr>
2019-10-03 14:47:50 +0200

    carte ok

--- src/app/carte/carte.component.ts
+++ src/app/carte/carte.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 import { CarteService } from '../services/carte.service';
 import { MesurePollution } from '../models/MesurePollution';
+import { StationDeMesurePollution } from '../models/StationDeMesurePollution';
 import { HttpErrorResponse } from '@angular/common/http';
 
+import "leaflet-mouse-position";
 
-
-declare var ol: any;
+type MesuresPollutionParStationDeMesure = Array<{
+  stationDeMesurePollution: StationDeMesurePollution,
+  listeDeMesurePollutionParStationDeMesure: MesurePollution[]
+}>;
 
 @Component({
   selector: 'app-carte',
@@ -14,76 +19,105 @@ declare var ol: any;
 })
 export class CarteComponent implements OnInit {
 
-  latitude: number = 18.5204;
-  longitude: number = 73.8567;
-  codeCommune: number = 44001;
-  map: any;
+  listeObjetsMesuresPollutionParStationDeMesure: MesuresPollutionParStationDeMesure = [];
+  codeCommune: number;
   listeDeMesurePollution: MesurePollution[];
-
-
-
+  listeDeStationDeMesure: StationDeMesurePollution[];
+latitude:number;
+longitude:number;
   constructor(private carteService: CarteService) { }
 
   ngOnInit() {
+    // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
+    const myfrugalmap = L.map('frugalmap').setView([47, 0], 12);
 
-    this.map = new ol.Map({
-      target: 'map',
-      layers: [
-        new ol.layer.Tile({
-          source: new ol.source.OSM()
-        })
-      ],
-      marker: [{ 18: 73 }],
-      view: new ol.View({
-        center: ol.proj.fromLonLat([0, 47]),
-        zoom: 8
-      })
-    });
+    const Coordinates = L.Control.extend({
+      onAdd: map => {
+        const container = L.DomUtil.create("div");
+        map.addEventListener("click", e => {
+
+            this.latitude=e.latlng.lat
+            this.longitude=e.latlng.lng
+       });
+       return container;
+     }
+  });
+  myfrugalmap.addControl(new Coordinates({ position: "bottomleft" }));
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: 'Frugal Map'
+    }).addTo(myfrugalmap);
+
+
 
     this.carteService.recupererMesures(this.codeCommune).subscribe((data: any) => {
       console.log(data);
       //this.subConnecte.next(true);
       this.listeDeMesurePollution = data;
-      console.log(this.listeDeMesurePollution);
+      //console.log(this.listeDeMesurePollution);
 
       for (const mesurePollution of this.listeDeMesurePollution) {
-        this.add_map_point(mesurePollution.stationDeMesure.latitude, mesurePollution.stationDeMesure.longitude);
+
+        let laStationDeMesureEstDejaEnregistre = false;
+
+        for (const objetMesuresPollutionParStationDeMesure of this.listeObjetsMesuresPollutionParStationDeMesure) {
+
+          if (objetMesuresPollutionParStationDeMesure.stationDeMesurePollution.id == mesurePollution.stationDeMesure.id) {
+
+            laStationDeMesureEstDejaEnregistre = true;
+            objetMesuresPollutionParStationDeMesure.listeDeMesurePollutionParStationDeMesure.push(mesurePollution)
+
+          }
+          console.log(this.listeObjetsMesuresPollutionParStationDeMesure)
+        }
+
+        if (!laStationDeMesureEstDejaEnregistre) {
+          this.listeObjetsMesuresPollutionParStationDeMesure.push(
+
+            {
+              stationDeMesurePollution: mesurePollution.stationDeMesure,
+              listeDeMesurePollutionParStationDeMesure: [mesurePollution]
+            }
+          )
+        }
+
+
+
       }
 
+      for (const objetMesuresPollutionParStationDeMesure of this.listeObjetsMesuresPollutionParStationDeMesure) {
+
+        const myIcon = L.icon({
+
+          iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png',
+          iconAnchor:   [10, 30],
+          iconSize:[20,30]
+        });
+
+        let textePopUp: string='';
+        for (const mesurePollution of objetMesuresPollutionParStationDeMesure.listeDeMesurePollutionParStationDeMesure) {
+
+            textePopUp =textePopUp+ ` ${mesurePollution.typeDeDonnee} : ${mesurePollution.valeur} --`
 
 
+        }
 
-    }, (error: HttpErrorResponse) => {
-      console.log("error", error);
-      //this.subConnecte.next(false);
-    })
+        L.marker([objetMesuresPollutionParStationDeMesure.stationDeMesurePollution.latitude,
+          objetMesuresPollutionParStationDeMesure.stationDeMesurePollution.longitude],
+          { icon: myIcon }).bindPopup(textePopUp).addTo(myfrugalmap).openPopup();
+
+
+      }
+
+    }
+
+      , (error: HttpErrorResponse) => {
+        console.log("error", error);
+        //this.subConnecte.next(false);
+      })
+    }
+
+    truc(){
+      console.log(this.latitude + " " + this.longitude)
+    }
   }
-
-
-
-  add_map_point(lat, lng) {
-    var vectorLayer = new ol.layer.Vector({
-      source: new ol.source.Vector({
-        features: [new ol.Feature({
-          geometry: new ol.geom.Point(ol.proj.transform([parseFloat(lng), parseFloat(lat)], 'EPSG:4326', 'EPSG:3857')),
-        })]
-      }),
-      style: new ol.style.Style({
-        image: new ol.style.Icon({
-          anchor: [0.5, 0.5],
-          anchorXUnits: 'fraction',
-          anchorYUnits: 'fraction',
-          src: '//raw.githubusercontent.com/jonataswalker/map-utils/master/images/marker.png'
-        })
-      }),
-  });
-    this.map.addLayer(vectorLayer);
-
-
-}
-
-
-
-
-
-}
