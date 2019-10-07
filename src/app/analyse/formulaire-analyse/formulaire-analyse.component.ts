46858b31fa9a38df489961afb118236cdc968d12
Eloi Turpin <eloi.turpin@gmail.com>
2019-10-07 15:33:47 +0200

    ajout auto-completion dans creation favori pour la commune

--- src/app/analyse/formulaire-analyse/formulaire-analyse.component.ts
+++ src/app/analyse/formulaire-analyse/formulaire-analyse.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {CommunesService} from '../../services/communes.service';
+import { CommunesService } from '../../services/communes.service';
 import Commune from '../../model/Commune';
-import {Observable} from 'rxjs';
-import {flatMap, map} from 'rxjs/operators';
-import {MesuresService} from '../../services/mesures.service';
-import {NgbCalendar, NgbCalendarGregorian, NgbCalendarHebrew, NgbDate, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
+import { Observable } from 'rxjs';
+import { flatMap, map } from 'rxjs/operators';
+import { MesuresService } from '../../services/mesures.service';
+import { NgbCalendar, NgbCalendarGregorian, NgbCalendarHebrew, NgbDate, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-formulaire-analyse',
@@ -22,19 +22,19 @@ export class FormulaireAnalyseComponent implements OnInit {
   /**
    * liste des indicateur, la valeur correspond à l’envoi au back, et le nom au front
    */
-  listeIndicateur = [{valeur: 'temperature', nom: 'Temperature'},
-    {valeur: 'pressure', nom: 'Pression' },
-    {valeur: 'humidity', nom: 'Humidité' },
-    {valeur: 'tempMin', nom: 'Temperature minimum' },
-    {valeur: 'tempMax', nom: 'Temperature maximum' },
-    {valeur: 'windSpeed', nom: 'Vitesse du vent' },
-    {valeur: 'windDegrees', nom: 'Orientation du vent'},
-    {valeur: 'O3', nom: 'O3' },
-    {valeur: 'PM10', nom: 'PM10' },
-    {valeur: 'PM25', nom: 'PM2.5' },
-    {valeur: 'NO2', nom: 'NO2' },
-    {valeur: 'SO2', nom: 'SO2' },
-    {valeur: 'CO', nom: 'CO' }];
+  listeIndicateur = [{ valeur: 'temperature', nom: 'Temperature' },
+  { valeur: 'pressure', nom: 'Pression' },
+  { valeur: 'humidity', nom: 'Humidité' },
+  { valeur: 'tempMin', nom: 'Temperature minimum' },
+  { valeur: 'tempMax', nom: 'Temperature maximum' },
+  { valeur: 'windSpeed', nom: 'Vitesse du vent' },
+  { valeur: 'windDegrees', nom: 'Orientation du vent' },
+  { valeur: 'O3', nom: 'O3' },
+  { valeur: 'PM10', nom: 'PM10' },
+  { valeur: 'PM25', nom: 'PM2.5' },
+  { valeur: 'NO2', nom: 'NO2' },
+  { valeur: 'SO2', nom: 'SO2' },
+  { valeur: 'CO', nom: 'CO' }];
   /**
    * parametre de recherche: date de debut de l’analyse
    */
@@ -64,31 +64,31 @@ export class FormulaireAnalyseComponent implements OnInit {
    * fonction permettant d’afficher uniquement le nom de la commune dans l’autocompletion et l’input
    * @param commune la commune dont on doit recuperer le nom
    */
- recupererNomCommune(commune: Commune) {
+  recupererNomCommune(commune: Commune) {
     return commune.nom;
-}
+  }
 
   /**
    * fonction permettant de faire la recherche de données pour l’analyse
    */
   rechercher() {
-   this.mesuresService.recupererAnalyses(this.communeRecupere.codeCommune, this.indicateur, new Date(this.dateDebut.setHours( 0, 0, 0, 0)), new Date(this.dateFin.setHours(23, 59, 59, 999)))
-     .subscribe(() => {this.messageError = undefined; },
-       (err) => {
-       if (err.status === 500) {
-       this.messageError = 'Pas de données disponible pour cette recherche.';
-     } else {
-         this.messageError = 'Impossible de récuperer les données de votre recherche';
-       }
-     });
-}
+    this.mesuresService.recupererAnalyses(this.communeRecupere.codeCommune, this.indicateur, new Date(this.dateDebut.setHours(0, 0, 0, 0)), new Date(this.dateFin.setHours(23, 59, 59, 999)))
+      .subscribe(() => { this.messageError = undefined; },
+        (err) => {
+          if (err.status === 500) {
+            this.messageError = 'Pas de données disponible pour cette recherche.';
+          } else {
+            this.messageError = 'Impossible de récuperer les données de votre recherche';
+          }
+        });
+  }
 
   /**
    * fonction retournant la date du jour
    */
   today(): NgbDateStruct {
-    return  new NgbCalendarGregorian().getToday();
-}
+    return new NgbCalendarGregorian().getToday();
+  }
   ngOnInit() {
   }
 
